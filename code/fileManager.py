d4fcc3de9c192a522af32ca57ac7189e204a731f
Dorota W?s <dorota.was@gmail.com>
2010-12-29 18:32:00 +0100

    SW Protocol

---
+++ code/fileManager.py
@@ -0,0 +1,99 @@
+# coding=utf-8
+
+import threading
+import os
+
+class FileInfo :
+    '''Klasa odpowiedzialna za dostarczanie informacji o plikach'''
+    def __init__(self) :
+        self.modifyTime = 0 
+        self.size = 0 
+        self.currentSize = 0
+        self.filename = ""
+        self.fileModified = threading.Condition()
+        self.fileType = "not found"
+
+    def setModifyTime(self, newTime):
+        global fileManager
+        self.modifyTime = newTime
+        with fileManager.fileInfoLock:
+            if (self.filename not in fileManager.fileInfo 
+                    or self.modifyTime > fileManager.fileInfo[self.filename].modifyTime):
+                fileManager.fileInfo[self.filename] = self
+
+    def sizeChanged (self, newSize) :
+        """ Zmienia currentSize i robi fileModified.notifyAll() """
+        self.fileModified.acquire()
+        self.currentSize = newSize
+        self.fileModified.notifyAll()
+        self.fileModified.release()
+
+class FileManager :
+    '''Klasa zapewniająca dostęp do plików (singleton)'''
+    def __init__(self) :
+        self.hiddenServerConnections = {} # słownik: nazwa użytkownika (string) -> HiddenServerConnection
+        self.requestInfo = {} # słownik: idZapytania(int) -> FileInfo
+        self.fileInfo = {} # słownik: ścieżka do pliku (string) -> FileInfo o najpóźniejszym czasie modyfikacji
+        self.lastRequestId = 0
+        self.fileInfoLock = threading.Lock()
+
+    def nextRequestId(self):
+        self.lastRequestId += 1
+        return self.lastRequestId
+
+    def getFileInfo (self, path, originalRequest) :
+        """ zwraca FileInfo odpowiedniego plkiu """
+        info = FileInfo()
+        if path.strip() == "/":
+            f = open("users","w")
+            for user in self.hiddenServerConnections.keys():
+                f.write(user+"\n")
+            f.close()
+            info.filename = "users"
+            info.fileType = "directory"
+            info.size = info.currentSize = os.path.getsize(info.filename)
+        else:
+            try:
+                path = path[1:]
+                if path[-1] == '/':
+                    path = path[:-1]
+                slash = path.find('/')
+                user = path[:slash] if slash != -1 else path
+                filename = path[slash+1:] if slash != -1 else ""
+                id = self.nextRequestId()
+                self.requestInfo[id] = FileInfo()
+                self.requestInfo[id].filename = (user+"/"+filename).replace("/",'.')
+                self.requestInfo[id].size = -1
+                print("getFileInfo({0}/{1}), sending request".format(user,filename))
+                cond = threading.Condition()
+                cond.acquire()
+                self.hiddenServerConnections[user].requestQueue.put({
+                    'filename':filename,
+                    'id':id,
+                    'originalRequest':originalRequest,
+                    'answerCondition':cond})
+                cond.wait()
+                cond.release()
+                print("getFileInfo({0}/{1}), request completed".format(user,filename))
+                info = self.requestInfo[id]
+                #Wait for PushFileConnection to establish:
+                info.fileModified.acquire()
+                if info.size == -1:
+                    info.fileModified.wait()
+                info.fileModified.release()
+            except BaseException as e:
+                import traceback
+                traceback.print_exc()
+                return FileInfo()
+        print("getFileInfo({0}):{1}".format(path,str(info)))
+        print(self.fileInfo)
+        return info
+
+    def processResponse (self, requestId, HSresponse, fileInfo) :
+        """ Przetwarza odpowiedź od HiddenServera i uaktualnia odpowiednie struktury """
+        if HSresponse['response'] == 'OK':
+            self.requestInfo[requestId] = fileInfo
+        elif HSresponse['response'] == 'OLD':
+            self.requestInfo[requestId].setModifyTime(int(HSresponse['modifytime']))
+
+fileManager = FileManager()
---
+++ code/hsConnection.py
@@ -0,0 +1,196 @@
+# coding=utf-8
+
+from fileManager import fileManager
+import fileManager as fm
+import threading
+import asynchat, asyncore
+import socket
+from Queue import Queue
+import base64
+
+class HiddenServerConnection(asynchat.async_chat):
+    '''Klasa reprezentująca trwałe połączenie HiddenServera z Serverem'''
+    dbg = True
+    responses = ["OK", "OLD", "MYNAMEIS"]
+    def __init__(self, sock):
+        asynchat.async_chat.__init__(self, sock)
+        self.requestQueue = Queue()
+        self.sendRequests = Queue()
+        self.set_terminator("\r\n")
+        self.data = []
+        self.response = {}
+        self.user = ""
+        self.writeThread = threading.Thread(target=self.writeLoop)
+        self.writeThread.daemon = True
+        self.writeThread.start()
+
+    def writeLoop(self):
+        global fileManager
+        while True:
+            request = self.requestQueue.get()
+            filename = (self.user+"/"+request['filename']).replace("/",".")
+            if filename in fileManager.fileInfo:
+                info = fileManager.fileInfo[filename]
+            else:
+                info = fm.FileInfo()
+            request['modifyTime'] = info.modifyTime
+            print(self.user+str(request))
+            self.sendRequests.put((request, info))
+            self.push("GET\n")
+            self.push("filename:{filename}\nmodifyTime:{modifyTime}\nid:{id}\noriginalRequest:{0}\n".format(
+                base64.b64encode(request['originalRequest']), **request) )
+
+    def collect_incoming_data(self, data):
+        #print "DATA: "+data
+        self.data.append(data)
+
+    def processResponse(self):
+        global fileManager
+        r = self.response
+        self.response = {}
+        if HiddenServerConnection.dbg:
+            print(r)
+        if r['response'] == "MYNAMEIS":
+            self.user = r['username'].strip()
+            fileManager.hiddenServerConnections[self.user] = self
+            return
+        else:
+            request, info = self.sendRequests.get()
+            fileManager.processResponse(request['id'], r, info)
+            c = request['answerCondition']
+            c.acquire()
+            c.notify()
+            c.release()
+
+    def handle_error(self):
+        pass
+
+    def found_terminator(self):
+        data = "".join(self.data)
+        self.data = []
+        if data.strip() == "":
+            self.processResponse()
+            return
+        if data.strip() in HiddenServerConnection.responses:
+            self.response['response'] = data.strip()
+        else:
+            colon = data.find(":")
+            field = data[:colon].lower().strip()
+            value = data[colon+1:]
+            self.response[field] = value
+
+    def handle_close(self):
+        print "Close"
+        self.close()
+
+class HSServer(asyncore.dispatcher):
+    '''Klasa odpowiedzialna za tworzenie HiddenServerConnectionów'''
+
+    def __init__(self, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.bind(("", port))
+        self.listen(5)
+
+    def handle_accept(self):
+        p = self.accept()
+        if not p:
+            return
+        conn, addr = p
+        h = HiddenServerConnection(conn)
+        
+def startHSServer():
+    s = HSServer(8888)
+    print("Starring HSServer.")
+    try:
+        asyncore.loop()
+    except BaseException,e:
+        s.close()
+        for c in fileManager.hiddenServerConnections.values():
+            c.handle_close()
+        asyncore.close_all()
+        print("Closing HSServer.")
+        raise e
+
+class PushFileConnection(asynchat.async_chat):
+    '''Klasa reprezentująca połączenie przesyłające plik z HiddenServera do Servera.
+Zapisuje dane do odpowiedniego pliku i przy każdym zapisie wywołuje sizeChanged()'''
+    dbg = True
+    def __init__(self, sock):
+        asynchat.async_chat.__init__(self, sock)
+        self.BUFFER_SIZE = 10
+        self.set_terminator("\n")
+        self.data = []
+        self.header = {}
+        self.reciveData = False
+        self.recived = 0
+        self.file = None
+        self.fileInfo = None
+
+    def collect_incoming_data(self, data):
+        self.data.append(data)
+
+    def handle_error(self):
+        import traceback
+        traceback.print_exc()
+
+
+    def formatHeader(self):
+        for key in ('length','id'):
+            self.header[key] = int(self.header[key])
+        self.header['filetype'] = self.header['filetype'].strip()
+        if PushFileConnection.dbg:
+            print self.header
+    @property
+    def length(self):
+        return self.header['length']
+
+    def found_terminator(self):
+        global fileManager
+        data = "".join(self.data)
+        self.data = []
+        if self.reciveData:
+            self.recived += len(data)
+            self.file.write(data)
+            self.file.flush()
+            self.fileInfo.sizeChanged(self.recived)
+            self.set_terminator(min(self.BUFFER_SIZE,self.length - self.recived))
+            return
+        colon = data.find(':')
+        field = data[:colon].lower().strip()
+        value = data[colon+1:]
+        if field == "":
+            self.formatHeader()
+            self.set_terminator(min(self.BUFFER_SIZE,self.length - self.recived))
+            self.fileInfo = fileManager.requestInfo[self.header['id']]
+            self.fileInfo.fileType = self.header['filetype']
+            self.fileInfo.size = self.header['length']
+            self.file = open(self.fileInfo.filename, 'w')
+            self.reciveData = True
+        else:
+            self.header[field] = value
+
+    def handle_close(self):
+        if PushFileConnection.dbg:print "PFC:Close"
+        self.close()
+        
+class PushFileServer(asyncore.dispatcher):
+    '''Klasa odpowiedzialna za tworzenie PushFileConnectionów'''
+
+    def __init__(self, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.bind(("", port))
+        self.listen(5)
+
+    def handle_accept(self):
+        p = self.accept()
+        if not p:
+            return
+        conn, addr = p
+        PushFileConnection(conn)
+        
+def startPushFileServer():
+    print("Starting PushFileServer")
+    s = PushFileServer(9999) 
+    asyncore.loop()
