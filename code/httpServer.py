d1c627dc13c05561c5b54259444aa66b557c54b3
Micha? Sapalski <sapalskimichal@gmail.com>
2011-01-10 21:57:29 +0100

    Poprawiony httpServer (nadal nie do ko?ca radzi sobie ze streamowaniem mp3 na chrome, ale ma wi?cej funkcji ni? wcze?niej). Wykomentowana cz??? debugu.

--- code/httpServer.py
+++ code/httpServer.py
@@ -10,8 +10,19 @@ class HttpRequest(BaseHTTPRequestHandler):
     '''Klasa odpowiedzialna za obsługę HTTP'''
     def handle_error(self):
         print("HTTP:ERROR")
+
+    def do_POST(self):
+        """Obsługa POST"""
+        self.answerGET()
     def do_GET(self):
         """Obsługa GET"""
+        self.answerGET()
+    def do_HEAD(self):
+        """Obsługa HEAD"""
+        self.answerGET(headerOnly=True)
+    def answerGET(self, headerOnly=False):
+        """Wyślij odpowiedni nagłówek i plik.
+        Jeśli opcja headerOnly jest ustawiona -- sam nagłówek"""
         info = FileInfo()
         try:
             info = fileManager.getFileInfo(unquote(self.path), 
@@ -19,52 +30,98 @@ class HttpRequest(BaseHTTPRequestHandler):
                         self.path,
                         self.request_version,
                         str(self.headers)]))
-            debug("HTTP:info:{0}".format(info))
+            #print("HTTP:info:{0}/{1}".format(info.filename, info.fileType))
+            #print(self.headers)
             if info.fileType == "not found":
                 raise IOError()
-            self.send_response(200)
+            begin = 0
+            end = info.size
+            partial = False
+            if "range" in self.headers:
+                begin,end = map(int,self.headers["range"].partition('=')[2].split('-'))
+                partial = True
+                #print("LOL:{0} {1}".format(begin,end))
+            if partial:
+                self.send_response(206)
+            else:
+                self.send_response(200)
             type = guess_type(info.filename)[0]
             if type is None or info.fileType == "directory":
-                type = 'text/html'
-            self.send_header('Content-type', type) #TODO
+                type = 'text/html; charset=UTF-8'
+            self.send_header('Content-type', type)
+            length = end - begin
+            size = info.size
+            if info.fileType == "directory":
+                listing = self.getDirectoryListing(self.path, info)
+                size = len(listing)
+            if partial:
+                self.send_header('Content-Range:', 'bytes {0}-{1}/{2}'.format(begin, end, size))
+            self.send_header('Content-Length', length)
+            self.send_header('Connection', 'close')
+            self.send_header('Accept-Ranges', 'bytes')
             self.end_headers()
-            f = open(info.filename, 'r')
+            if headerOnly:
+                return
             if info.fileType == "file":
+                f = open(info.filename, 'r')
                 written = 0
-                chunkSize = 1024
+                chunkSize = 16*1024
                 while written < info.size:
                     if written == info.currentSize:
                         with info.fileModified:
                             info.fileModified.wait()
                     chunk = f.read(min(chunkSize, info.currentSize-written))
+                    pbegin = max(0, min(begin-written, len(chunk)))
+                    pend = min(len(chunk), max(end-written, 0))
+                    part = chunk[pbegin:pend]
                     written += len(chunk)
-                    self.wfile.write(chunk)
+                    if len(part) > 0:
+                        self.wfile.write(part)
+                    #print("WRITTEN: {0} [{1}:{2}]".format(written, pbegin,pend))
+                    if written > end:
+                        break
                     #debug("HTTP: "+str(written))
                     #debug(chunk)
             elif info.fileType == "directory":
-                if self.path[-1] == '/':
-                    self.path = self.path[:-1]
-                while info.currentSize < info.size:
-                    with info.fileModified:
-                        info.fileModified.wait()
-                self.wfile.write('''<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8"/>\n
-                <title>[wikiserver]:{0}</title></head>\n
-                <body><h1>[wikiserver]:{0}</h1>'''.format(unquote(self.path)))
-                if self.path != '':
-                    self.wfile.write('<h3><a href="{0}/..">do góry</a></h3>\n'.format(self.path))
-                self.wfile.write('<h2>{0}:</h2><ul>\n'.format(("Pliki" if self.path != '' else "Użytkownicy")))
-
-                for line in sorted(f):
-                    self.wfile.write('<li><a href="{0}/{2}">{1}</a></li>\n'.format(self.path, line.strip(), quote(line.strip())))
-                self.wfile.write("</body></html>\n")
-        except IOError:
-            self.send_error(404,'Nie znaleziono pliku {0}'.format(self.path))
-        except BaseException as e:
-            print(e)
+                self.wfile.write(listing)
+        except IOError :
+            debug(" ".join(["Błąd:",self.command, self.path, self.request_version, str(self.headers)]))
+            import traceback
+            debug(traceback.format_exc())
+            try:
+                self.send_error(404,'Nie znaleziono pliku {0}'.format(self.path))
+            except BaseException :
+                debug("Nie udało się wysłać odpowiedzi")
+            debug("[/Błąd]")
+        except BaseException :
+            import traceback
+            debug(traceback.format_exc())
         finally:
-            self.wfile.flush()
-            self.wfile.close()
+            #self.wfile.flush()
+            #self.wfile.close()
             info.stopUsing()
+
+    def getDirectoryListing(self, path, fileInfo):
+        """Zwraca listing katalogu opisanego za pomocą fileInfo.
+        path to ścieżka (bezwzględna) do tego katalogu."""
+        f = open(fileInfo.filename, 'r')
+        res = []
+        if path[-1] == '/':
+            path = path[:-1]
+        while fileInfo.currentSize < fileInfo.size:
+            with fileInfo.fileModified:
+                fileInfo.fileModified.wait()
+        res.append('''<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8"/>\n
+        <title>[wikiserver]:{0}</title></head>\n
+        <body><h1>[wikiserver]:{0}</h1>'''.format(unquote(path)))
+        if path != '':
+            res.append('<h3><a href="{0}/..">do góry</a></h3>\n'.format(path))
+        res.append('<h2>{0}:</h2><ul>\n'.format(("Pliki" if path != '' else "Użytkownicy")))
+
+        for line in sorted(f):
+            res.append('<li><a href="{0}/{2}">{1}</a></li>\n'.format(path, line.strip(), quote(line.strip())))
+        res.append("</body></html>\n")
+        return "".join(res)
 
 class HttpServer(ThreadingMixIn, HTTPServer):
     '''Klasa odpowiedzialna za tworzenie HttpRequestów'''
