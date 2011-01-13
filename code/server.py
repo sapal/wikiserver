14ffac7b65e6c0c04625fc77e8a819f14951160f
Micha? Sapalski <sapalskimichal@gmail.com>
2011-01-13 10:06:53 +0100

    Doda?em opcje do servera. W zwi?zku z tym, ?e katalog cache jest tworzony je?eli nie istnieje, to usun??em go z repozytorium.

--- code/server.py
+++ code/server.py
@@ -5,8 +5,23 @@ import threading
 from fileManager import fileManager
 import httpServer
 import hsConnection
+from optparse import OptionParser
+import config
+import os
 
 if __name__=="__main__":
+    parser = OptionParser()
+    parser.add_option('-p', '--port', dest='httpPort', help='port for http server', default=8080, type="int")
+    parser.add_option('-c', '--cache-directory', dest='cacheDir', help='directory for storing temporary files (cache)', default='cache')
+    parser.add_option('-s', '--cache-max-size', dest='cacheMaxSize', help='maximum size of temporary files (in KB). Note that actual disk usage may be higher in some special cases (high server load)', default=10*1024, type="int")
+    options, args = parser.parse_args()
+    config.httpPort = options.httpPort
+    config.cacheDir = options.cacheDir
+    if config.cacheDir[-1] == '/':
+        config.cacheDir = config.cacheDir[:-1]
+    os.makedirs(config.cacheDir)
+    config.cacheMaxSize = options.cacheMaxSize*1024
+
     http = threading.Thread(target=httpServer.start)
     http.daemon = True
     http.start()
