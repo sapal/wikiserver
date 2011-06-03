1155539de6302733ad7cf2138d4ef1bbed591bcb
mib <mib@v-lo.krakow.pl>
2011-06-03 13:16:41 +0200

    Fix dla pythona 2.7

--- code/ssl_asyncchat.py
+++ code/ssl_asyncchat.py
@@ -4,6 +4,9 @@ import asynchat, asyncore
 import socket
 import ssl
 import errno
+import select
+
+from time import sleep
 
 def _log(msg):
     print "[SSLSupport::] ", msg
@@ -13,38 +16,62 @@ class SSLAsyncChat(asynchat.async_chat):
     def connect(self, host):
         """ Mapuje metody send i recv na uwzgledniajace SSL. """
         _log("Remapping recv & send")
-        self.send = self._send
-        self.recv = self._recv
+        self.send = self._mysend
+        self.recv = self._myrecv
         asynchat.async_chat.connect(self, host)
 
     """ Inicjalizuje wsparcie SSL po stronie serwera. """
     def init_server_side(self):
         _log("server socket wrapping")
-        self.ssl = ssl.wrap_socket(self.socket, server_side=True, certfile='server.crt', keyfile='server.key')
+        self.ssl = ssl.wrap_socket(self.socket, server_side=True, certfile='server.crt', keyfile='server.key', do_handshake_on_connect=False)
+        while True:
+            try:
+                self.ssl.do_handshake()
+                break
+            except ssl.SSLError as err:
+                if err.args[0] == ssl.SSL_ERROR_WANT_READ:
+                    select.select([self.ssl], [], [])
+                elif err.args[0] == ssl.SSL_ERROR_WANT_WRITE:
+                    select.select([], [self.ssl], [])
+                else:
+                    raise
         self.set_socket(self.ssl)
-        self.send = self._send
-        self.recv = self._recv
-
+        self.send = self._mysend
+        self.recv = self._myrecv
+        
     def handle_connect(self):
         """ Obkłada socket klienta protokołem SSL. """
         _log("Switching transmission to SSL (ClientSide)")
-        self.ssl = ssl.wrap_socket(self.socket, cert_reqs=ssl.CERT_NONE)
+        self.ssl = ssl.wrap_socket(self.socket, cert_reqs=ssl.CERT_NONE, do_handshake_on_connect=False)
         self.set_socket(self.ssl)
- 
-    def _send(self, data):
+        while True:
+            try:
+                self.ssl.do_handshake()
+                break
+            except ssl.SSLError as err:
+                if err.args[0] == ssl.SSL_ERROR_WANT_READ:
+                    select.select([self.ssl], [], [])
+                elif err.args[0] == ssl.SSL_ERROR_WANT_WRITE:
+                    select.select([], [self.ssl], [])
+                else:
+                    raise
+
+    def _mysend(self, data):
         """ Odpowiednik send() dla połączeń SSL """
         _log("SSL send")
         try:
             result = self.write(data)
             return result
         except ssl.SSLError, why:
+            _log(why)
+            _log(why.args)
             if why[0] in (asyncore.EWOULDBLOCK, errno.ESRCH):
                 return 0
             else:
                 raise ssl.SSLError, why
             return 0
  
-    def _recv(self, buffer_size):
+    def _myrecv(self, buffer_size):
         """ Odpowiednik recv() dla połączeń SSL """
         _log("SSL recv")
         try:
@@ -54,6 +81,8 @@ class SSLAsyncChat(asynchat.async_chat):
                 return ''
             return data
         except ssl.SSLError, why:
+            _log(why)
+            _log(why.args)
             if why[0] in (asyncore.ECONNRESET, asyncore.ENOTCONN, 
                           asyncore.ESHUTDOWN):
                 self.handle_close()
