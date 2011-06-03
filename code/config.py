7cf88d8bd8ca2496afffc48fad09e4cb05ae9b4a
mib <mib@v-lo.krakow.pl>
2011-06-03 13:51:21 +0200

    SSL dla Push File

--- code/config.py
+++ code/config.py
@@ -8,3 +8,11 @@ cacheDir = 'cache'
 cacheMaxSize = 100*1024**2 # 100 MB
 _f = os.path.abspath(__file__)
 dataDir = _f[:_f.rfind(os.sep)-4] + 'data'
+
+# dostarczane z klientem
+SSL_C_CACERTS = 'ca_certs'
+
+# dostarczane z serwerem
+SSL_S_CERTFILE = 'server.crt'
+SSL_S_KEYFILE = 'server.key'
+
